2cb8adb6bb4f1f126b76aaa34da96c3408ca7f1a
Evan Coulson <evan.m.coulson@gmail.com>
2023-08-27 23:49:08 -0700

    Calculating hydrogen production

--- policy_modeling/main.py
+++ policy_modeling/main.py
@@ -1,15 +1,27 @@
 from datetime import datetime
 from model import Electrolyzer, PowerPlant, \
     EnergySource, consume, ConsumptionUnit, \
-    generate, get_energy_source_portfolios
+    generate, get_energy_source_portfolios, calculate_emitted_co2, \
+    ConstantProduction, purchase, calculate_hydrogen_produced, \
+    calculate_tax_credit
 
 
 def main():
+    simulation_id = 0
     powerplant = PowerPlant(
         plant_id=50098,
         energy_source=EnergySource.ENERGY_SOURCE_NATURAL_GAS,
     )
-    electrolyzer = Electrolyzer()
+    electrolyzer = Electrolyzer(
+        id=0,
+        replacement_threshold=0.8,
+        degredation_rate=0.02,
+        capacity_kw=1000,
+        production_method=ConstantProduction(conversion_rate=45),
+        capital_expenditure=1150,
+        operation_expenditure=17,
+        replacement_cost=0.5
+    )
 
     current_timestep = datetime(
         year=2023,
@@ -22,21 +34,59 @@ def main():
                 month=7,
                 day=1
             ))
+    generate(powerplant, 1_351.545, datetime(
+        year=2023,
+        month=7,
+        day=1
+    ), sale_price=2)
     consume(powerplant, 15_706.68,
             ConsumptionUnit.CONSUMPTION_UNIT_MMBTU, datetime(
                 year=2023,
                 month=8,
                 day=1
             ))
-    generate(powerplant, 1_351.545, current_timestep)
+    generate(powerplant, 1_351.545, datetime(
+        year=2023,
+        month=8,
+        day=1
+    ), sale_price=2)
+    energy_transactions = [
+        purchase(simulation_id, electrolyzer,
+                 powerplant, 20000, current_timestep)
+    ]
+    portfolios = get_energy_source_portfolios(energy_transactions)
+    emissions = calculate_emitted_co2(electrolyzer, portfolios)
+    hydrogen_productions = calculate_hydrogen_produced(
+        electrolyzer, portfolios)
 
-    portfolios = get_energy_source_portfolios([powerplant], current_timestep)
+    print("-==- ELECTROLYZER -==-")
+    print(electrolyzer.__dict__)
+    print("----------------------")
 
-    print(
-        portfolios[0].__dict__,
-        portfolios[1].__dict__,
-        electrolyzer
-    )
+    print("-==- TRANSACTIONS -==-")
+    for transaction in energy_transactions:
+        print(transaction.__dict__)
+    print("----------------------")
+
+    print("-==-  PORTFOLIOS  -==-")
+    for portfolio in portfolios:
+        print(portfolio.__dict__)
+    print("----------------------")
+
+    print("-==-  EMISSIONS   -==-")
+    for emission in emissions:
+        print(emission.__dict__)
+    print("----------------------")
+
+    print("-==-   HYDROGEN   -==-")
+    for hydrogen_production in hydrogen_productions:
+        print(hydrogen_production.__dict__)
+    print("----------------------")
+
+    tax_credit = calculate_tax_credit(emissions, hydrogen_productions)
+    print("-==- TAX CREDIT  -==-")
+    print(tax_credit.__dict__)
+    print("----------------------")
 
 
 main()
--- policy_modeling/model.py
+++ policy_modeling/model.py
@@ -24,6 +24,20 @@ class ConsumptionUnit(Enum):
     CONSUMPTION_UNIT_MMBTU = 2
 
 
+class PPAAgreement:
+    plant_id: int
+    electrolyzer_id: int
+
+
+class PolicyType(Enum):
+    POLICY_TYPE_PPA_AGREEMENT = 1
+
+
+class Policy:
+    policy_type: PolicyType
+    ppa_agreement: PPAAgreement
+
+
 class ConsumptionMetric:
     plant_id: int
     time_consumed: datetime
@@ -31,9 +45,9 @@ class ConsumptionMetric:
 
     def __init__(
         self,
-        plant_id: int,
-        time_consumed: datetime,
-        amount_mmbtu: float
+        plant_id: int = 0,
+        time_consumed: datetime = datetime.now(),
+        amount_mmbtu: float = 0
     ):
         self.plant_id = plant_id
         self.time_consumed = time_consumed
@@ -44,16 +58,19 @@ class GenerationMetric:
     plant_id: int
     time_generated: datetime
     amount_mwh: float
+    sale_price_usd_per_mwh: float
 
     def __init__(
         self,
-        plant_id: int,
-        time_generated: datetime,
-        amount_mwh: float
+        plant_id: int = 0,
+        time_generated: datetime = datetime.now(),
+        amount_mwh: float = 0,
+        sale_price_usd_per_mwh: float = 0
     ):
         self.plant_id = plant_id
         self.time_generated = time_generated
         self.amount_mwh = amount_mwh
+        self.sale_price_usd_per_mwh = sale_price_usd_per_mwh
 
 
 class PowerPlant:
@@ -64,8 +81,8 @@ class PowerPlant:
 
     def __init__(
         self,
-        plant_id: int,
-        energy_source: EnergySource,
+        plant_id: int = 0,
+        energy_source: EnergySource = EnergySource.ENERGY_SOURCE_COAL,
         consumption: list[ConsumptionMetric] = list(),
         generation: list[GenerationMetric] = list()
     ):
@@ -73,6 +90,194 @@ class PowerPlant:
         self.energy_source = energy_source
         self.consumption = consumption
         self.generation = generation
+
+
+class EnergyTransaction:
+    simulation_id: int
+    electrolyzer_id: int
+    plant_id: int
+    timestamp: datetime
+    amount_mwh: float
+    fuel_consumed_mmbtu: float
+    price_usd: float
+    energy_source: EnergySource
+
+    def __init__(
+        self,
+        simulation_id: int = 0,
+        electrolyzer_id: int = 0,
+        plant_id: int = 0,
+        timestamp: datetime = datetime.now(),
+        amount_mwh: float = 0,
+        fuel_consumed_mmbtu: float = 0,
+        price_usd: float = 0,
+        energy_source: EnergySource = EnergySource.ENERGY_SOURCE_COAL
+    ) -> None:
+        self.simulation_id = simulation_id
+        self.electrolyzer_id = electrolyzer_id
+        self.plant_id = plant_id
+        self.timestamp = timestamp
+        self.amount_mwh = amount_mwh
+        self.price_usd = price_usd
+        self.energy_source = energy_source
+        self.fuel_consumed_mmbtu = fuel_consumed_mmbtu
+
+
+class EmissionEvent:
+    electrolyzer_id: int
+    emission_timestamp: datetime
+    amount_emitted_kg: float
+
+    def __init__(
+        self,
+        electrolyzer_id: int = 0,
+        emission_timestamp: datetime = datetime.now(),
+        amount_emitted_kg: float = 0
+    ) -> None:
+        self.electrolyzer_id = electrolyzer_id
+        self.emission_timestamp = emission_timestamp
+        self.amount_emitted_kg = amount_emitted_kg
+
+
+class HydrogenProduction:
+    plant_id: int
+    production_timestamp: datetime
+    kg_hydrogen: float
+
+    def __init__(
+        self,
+        plant_id: int = 0,
+        production_timestamp: datetime = datetime.now(),
+        kg_hydrogen: float = 0
+    ) -> None:
+        self.plant_id = plant_id
+        self.production_timestamp = production_timestamp
+        self.kg_hydrogen = kg_hydrogen
+
+
+class Production:
+    # takes in the input kw to determine the efficiency rate
+    def calculate_production(self, _: float) -> float:
+        raise Exception("Use impl of production")
+
+
+class ConstantProduction(Production):
+    conversion_rate: float  # kWh / kg
+
+    def calculate_production(self, input_kwh: float) -> float:
+        return input_kwh / self.conversion_rate
+
+    def __init__(self, conversion_rate: float = 0) -> None:
+        super().__init__()
+        self.conversion_rate = conversion_rate
+
+
+class Electrolyzer:
+    id: int
+    replacement_threshold: float
+    degradation_rate: float
+    capacity_kw: float
+    production_method: Production
+    capital_expenditure: float
+    operational_expenditure: float
+    replacement_cost: float
+
+    def __init__(
+        self,
+        id: int = 0,
+        replacement_threshold: float = 0,
+        degredation_rate: float = 0,
+        capacity_kw: float = 0,
+        production_method: Production = Production(),
+        capital_expenditure: float = 0,
+        operation_expenditure: float = 0,
+        replacement_cost: float = 0
+    ) -> None:
+        self.id = id
+        self.replacement_threshold = replacement_threshold
+        self.degradation_rate = degredation_rate
+        self.capacity_kw = capacity_kw
+        self.production_method = production_method
+        self.capital_expenditure = capital_expenditure
+        self.operational_expenditure = operation_expenditure
+        self.replacement_cost = replacement_cost
+
+
+class TaxCredit45V:
+    amount_usd: float
+
+    def __init__(
+        self,
+        amount_usd: float = 0
+    ) -> None:
+        self.amount_usd = amount_usd
+
+
+class EnergySourcePortfolio:
+    timestamp: datetime
+    total_electricity_mwh: float
+    petroleum_mmbtu: float
+    hydrocarbons_mmbtu: float
+    natural_gas_mmbtu: float
+    coal_mmbtu: float
+    nuclear_mmbtu: float
+    solar_mmbtu: float
+    geothermal_mmbtu: float
+    wind_mmbtu: float
+    biomass_mmbtu: float
+    hydropower_mmbtu: float
+
+    def __init__(
+        self,
+        timestamp: datetime = datetime.now(),
+        total_electricity_mwh: float = 0,
+        petroleum_mmbtu: float = 0,
+        hydrocarbons_mmbtu: float = 0,
+        natural_gas_mmbtu: float = 0,
+        coal_mmbtu: float = 0,
+        nuclear_mmbtu: float = 0,
+        solar_mmbtu: float = 0,
+        geothermal_mmbtu: float = 0,
+        wind_mmbtu: float = 0,
+        biomass_mmbtu: float = 0,
+        hydropower_mmbtu: float = 0
+    ):
+        self.timestamp = timestamp
+        self.total_electricity_mwh = total_electricity_mwh
+        self.petroleum_mmbtu = petroleum_mmbtu
+        self.hydrocarbons_mmbtu = hydrocarbons_mmbtu
+        self.natural_gas_mmbtu = natural_gas_mmbtu
+        self.coal_mmbtu = coal_mmbtu
+        self.nuclear_mmbtu = nuclear_mmbtu
+        self.solar_mmbtu = solar_mmbtu
+        self.geothermal_mmbtu = geothermal_mmbtu
+        self.wind_mmbtu = wind_mmbtu
+        self.biomass_mmbtu = biomass_mmbtu
+        self.hydropower_mmbtu = hydropower_mmbtu
+
+
+def purchase(
+    simulation_id: int,
+    electrolyzer: Electrolyzer,
+    power_plant: PowerPlant,
+    amount_mwh: float,
+    timestamp: datetime,
+) -> EnergyTransaction:
+    for (i, generation) in enumerate(power_plant.generation):
+        if generation.time_generated.year == timestamp.year and \
+                generation.time_generated.month == timestamp.month:
+            consumption = power_plant.consumption[i]
+            return EnergyTransaction(
+                simulation_id=simulation_id,
+                electrolyzer_id=electrolyzer.id,
+                plant_id=power_plant.plant_id,
+                timestamp=timestamp,
+                amount_mwh=amount_mwh,
+                fuel_consumed_mmbtu=consumption.amount_mmbtu,
+                price_usd=generation.sale_price_usd_per_mwh * amount_mwh,
+                energy_source=power_plant.energy_source
+            )
+    raise Exception("No generation from powerplant at the current timestamp")
 
 
 def convert_to_mmbtu(amount: float, unit: ConsumptionUnit):
@@ -88,14 +293,15 @@ def convert_to_mmbtu(amount: float, unit: ConsumptionUnit):
 def generate(
     power_plant: PowerPlant,
     amount_mwh: float,
-    timestamp: datetime
+    timestamp: datetime,
+    sale_price: float
 ) -> None:
-    generation = GenerationMetric(
+    power_plant.generation.append(GenerationMetric(
         plant_id=power_plant.plant_id,
         amount_mwh=amount_mwh,
-        time_generated=timestamp
-    )
-    power_plant.generation.append(generation)
+        time_generated=timestamp,
+        sale_price_usd_per_mwh=sale_price
+    ))
 
 
 # TODO: Check for unique timestamp
@@ -113,115 +319,74 @@ def consume(
     power_plant.consumption.append(consumption)
 
 
-class EmissionEvent:
-    plant_id: int
-    emission_timestamp: datetime
-    amount_emitted_kg: float
-
-
-class HydrogenProduction:
-    plant_id: int
-    production_timestamp: datetime
-    kg_hydrogen: float
-
-
-class ProductionRate:
-    # takes in the input kw to determine the efficiency rate
-    def calculate_production_rate(self, input_kwh: float) -> float:
-        return 0
-
-
-class ConstantProductionRate(ProductionRate):
-    conversion_rate: float
-
-    # takes in the input kw to determine the efficiency rate
-    def calculate_production_rate(self, input_mwh: float) -> float:
-        return 1 / self.conversion_rate
-
-
-class Electrolyzer:
-    replacement_threshold: float
-    degradation_rate: float
-    capacity_kw: float
-    production_rate: ProductionRate
-    capital_expenditure: float
-    operational_expenditure: float
-    replacement_cost: float
-
-
-class TaxCredit45V:
-    amount_usd: float
-
-
-class EnergySourcePortfolio:
-    timestamp: datetime
-    petroleum_mmbtu: float
-    hydrocarbons_mmbtu: float
-    natural_gas_mmbtu: float
-    coal_mmbtu: float
-    nuclear_mmbtu: float
-    solar_mmbtu: float
-    geothermal_mmbtu: float
-    wind_mmbtu: float
-    biomass_mmbtu: float
-    hydropower_mmbtu: float
-
-    def __init__(self, timestamp: datetime):
-        self.timestamp = timestamp
-        self.petroleum_mmbtu = 0
-        self.hydrocarbons_mmbtu = 0
-        self.natural_gas_mmbtu = 0
-        self.coal_mmbtu = 0
-        self.nuclear_mmbtu = 0
-        self.solar_mmbtu = 0
-        self.geothermal_mmbtu = 0
-        self.wind_mmbtu = 0
-        self.biomass_mmbtu = 0
-        self.hydropower_mmbtu = 0
-
-
 def get_energy_source_portfolios(
-    power_plants: list[PowerPlant],
-    start_time: datetime
+    transactions: list[EnergyTransaction],
 ) -> list[EnergySourcePortfolio]:
-    portfolios = []
-    current_indexes: dict[int, int] = {}
-    last_time = start_time
+    portfolios_over_time: dict[datetime, EnergySourcePortfolio] = {}
+    portfolios: list[EnergySourcePortfolio] = []
 
-    for power_plant in power_plants:
-        for (i, consumption) in enumerate(power_plant.consumption):
-            last_time = max(start_time, consumption.time_consumed)
-            if consumption.time_consumed.year == start_time.year \
-                    and consumption.time_consumed.month == start_time.month:
-                current_indexes[power_plant.plant_id] = i
+    for transaction in transactions:
+        transaction_portfolio = portfolio_from_transaction(transaction)
+        if transaction.timestamp not in portfolios_over_time:
+            portfolios_over_time[transaction.timestamp] = transaction_portfolio
+        else:
+            portfolios_over_time[transaction.timestamp] = merge_portfolio(
+                transaction_portfolio,
+                portfolios_over_time[transaction.timestamp]
+            )
 
-    current_timestamp = start_time
-    while current_timestamp <= last_time:
-        portfolio = EnergySourcePortfolio(timestamp=current_timestamp)
-        for power_plant in power_plants:
-            consumption_index = current_indexes[power_plant.plant_id]
-            consumption = power_plant.consumption[consumption_index]
-            portfolio.natural_gas_mmbtu += consumption.amount_mmbtu
-            current_indexes[power_plant.plant_id] += 1
-        portfolios.append(portfolio)
-        current_timestamp = datetime(
-            year=current_timestamp.year,
-            month=current_timestamp.month + 1,
-            day=current_timestamp.day
-        )
+    for timestamp in portfolios_over_time.keys():
+        portfolios.append(portfolios_over_time[timestamp])
 
     return portfolios
 
 
+def portfolio_from_transaction(
+    transaction: EnergyTransaction
+) -> EnergySourcePortfolio:
+    portfolio = EnergySourcePortfolio(
+        timestamp=transaction.timestamp,
+        total_electricity_mwh=transaction.amount_mwh
+    )
+
+    match transaction.energy_source:
+        case EnergySource.ENERGY_SOURCE_NATURAL_GAS:
+            portfolio.natural_gas_mmbtu += transaction.fuel_consumed_mmbtu
+
+    return portfolio
+
+
+def merge_portfolio(
+    portfolio_a: EnergySourcePortfolio,
+    portfolio_b: EnergySourcePortfolio
+) -> EnergySourcePortfolio:
+    if portfolio_a.timestamp != portfolio_b.timestamp:
+        raise Exception("Timestamps must match")
+
+    merged_portfolio = EnergySourcePortfolio()
+    merged_portfolio.total_electricity_mwh = \
+        portfolio_b.total_electricity_mwh + portfolio_a.total_electricity_mwh
+    merged_portfolio.natural_gas_mmbtu = portfolio_b.natural_gas_mmbtu + \
+        portfolio_a.natural_gas_mmbtu
+
+    return merged_portfolio
+
+
 def calculate_emitted_co2(
-    portfolio: EnergySourcePortfolio,
-    timestamp: datetime
-) -> EmissionEvent:
-    emission = EmissionEvent()
-    emission.emission_timestamp = timestamp
-    emission.amount_emitted_kg += calculate_emitted_co2_from_natural_gas(
-        portfolio.natural_gas_mmbtu)
-    return emission
+    electrolyzer: Electrolyzer,
+    portfolios: list[EnergySourcePortfolio],
+) -> list[EmissionEvent]:
+    emissions = []
+    for portfolio in portfolios:
+        amount_emitted_kg = 0
+        amount_emitted_kg += calculate_emitted_co2_from_natural_gas(
+            portfolio.natural_gas_mmbtu)
+        emissions.append(EmissionEvent(
+            electrolyzer_id=electrolyzer.id,
+            emission_timestamp=portfolio.timestamp,
+            amount_emitted_kg=amount_emitted_kg
+        ))
+    return emissions
 
 
 def calculate_emitted_co2_from_natural_gas(natural_gas_mmbtu: float) -> float:
@@ -230,18 +395,19 @@ def calculate_emitted_co2_from_natural_gas(natural_gas_mmbtu: float) -> float:
 
 def calculate_hydrogen_produced(
     electrolyzer: Electrolyzer,
-    generations: list[GenerationMetric], timestamp: datetime
-) -> HydrogenProduction:
-    total_generated_electricity_mwh = sum(
-        map(lambda generation: generation.amount_mwh, generations))
+    portfolios: list[EnergySourcePortfolio],
+) -> list[HydrogenProduction]:
+    productions: list[HydrogenProduction] = []
 
-    production = HydrogenProduction()
-    production.production_timestamp = timestamp
-    production.kg_hydrogen = total_generated_electricity_mwh * \
-        electrolyzer.production_rate.calculate_production_rate(
-            total_generated_electricity_mwh)
+    for portfolio in portfolios:
+        production = HydrogenProduction()
+        production.production_timestamp = portfolio.timestamp
+        production.kg_hydrogen = portfolio.total_electricity_mwh * \
+            electrolyzer.production_method.calculate_production(
+                portfolio.total_electricity_mwh)
+        productions.append(production)
 
-    return production
+    return productions
 
 
 def calculate_tax_credit(
@@ -262,6 +428,6 @@ def calculate_tax_credit(
     elif 0.45 <= co2_per_h2 and co2_per_h2 < 1.5:
         credit.amount_usd = TAX_CREDIT_45V_MAX_VALUE_USD * 0.334
     elif co2_per_h2 < 0.45:
-        credit.amount_usd = TAX_CREDIT_45V_MAX_VALUE_USD * 0.334
+        credit.amount_usd = TAX_CREDIT_45V_MAX_VALUE_USD
 
     return credit
